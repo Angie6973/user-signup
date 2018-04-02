9041adfe4af98649ab59af6e3c0f2f9313ed221c
Angelika Stephanie Pierce <angiekinter@gmail.com>
2018-04-01 21:00:50 -0500

    started user-signup assignmnet

---
+++ main.py
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <style>
+    </style>
+        .error {color:red;}
+     </style>
+</head>
+<body>
+    <h1>Signup</h1>
+        <form method="post">
+        <table>
+            <tbody>
+            </tbody>
+        </table>
+        <input type="submit">
+        </form>
+</body>
+
+
+
+</html>
